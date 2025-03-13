6b098697b6f082344b8026ffa29e47a8120a36e2
Owen <118958358+owenarsene@users.noreply.github.com>
2025-03-13 11:10:36 +0800

    Update Google 帳號綁定.py

--- Google 帳號綁定.py
+++ Google 帳號綁定.py
@@ -16,3 +16,8 @@ def google_login():
         return redirect(url_for('dashboard'))
     flash('Google 登入失敗!', 'danger')
     return redirect(url_for('login'))
+
+#連結wix後端資料庫，向使用者索取使用者個資授權條款資料。儲存Gmail, password
+def forget_password():
+    if google.login input fail (n*3):
+        click_button do reset 
